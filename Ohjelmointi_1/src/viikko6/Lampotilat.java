700bc99fe1d8ab5595a60f43a586a354e7275cbc
bgz787@myy.haaga-helia.fi <bgz787@myy.haaga-helia.fi>
2024-02-29 15:01:02 +0200

    lampo

---
+++ Ohjelmointi_1/src/viikko6/Lampotilat.java
@@ -0,0 +1,27 @@
+package viikko6;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Lampotilat {
+    
+    public static void main(String[] args) {
+    int[] lampotilat;
+    System.out.println("Anna lämpötila: ");
+
+    }
+
+    int kysyLampotilat(int[] lampotilat, final int MAX) {
+        Scanner scanner = new Scanner(System.in);
+        int lampo;
+        for (int i= 1; i<=100; i++) {
+            System.out.println("Anna lämpötila: ");
+            lampo = scanner.nextInt();
+            
+        }
+    }
+
+    void naytaLampotilat(int[] lampotilat, int lkm) {
+
+
+    }
+}
